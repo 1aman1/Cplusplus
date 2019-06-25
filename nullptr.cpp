21b24309a1d4bed9d38ab359223753ad5252fe36
Aman Gupta <49602858+1aman1@users.noreply.github.com>
2019-06-25 14:47:44 +0530

    Create nullptr.cpp

---
+++ nullptr.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	double var = 4;
+	double* ptr = 0;
+	
+	if( ptr == 0 )
+		cout << "implicit nullptr conversion to bool";
+
+  //	system("pause");
+
+	return 0;
+}
