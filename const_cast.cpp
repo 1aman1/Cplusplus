b516e3accbd58c19a04696f70da7977a24fbccec
Aman Gupta <49602858+1aman1@users.noreply.github.com>
2019-07-03 12:04:22 +0530

    stripping constness

--- const_cast.cpp
+++ const_cast.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
   const int a = 5;
   int* q = const_cast<int *>(&a);
-  //stripped off constness using const_cast
+  //stripped off constness using const_cast 
   //allowing re-assignment to variable
   *q = 50;
   cout << a << " " << *q << endl;
