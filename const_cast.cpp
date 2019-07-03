ca0b12901ee091b8ef92fba5c79317fac98de910
Aman Gupta <49602858+1aman1@users.noreply.github.com>
2019-07-03 12:03:44 +0530

    Add files via upload

---
+++ const_cast.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  const int a = 5;
+  int* q = const_cast<int *>(&a);
+  //stripped off constness using const_cast
+  //allowing re-assignment to variable
+  *q = 50;
+  cout << a << " " << *q << endl;
+//	system("pause");
+	return 0;
+}
