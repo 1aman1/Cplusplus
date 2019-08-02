8a1985ced8afda99466a03fcda3a19dd8ba6153c
Aman Gupta <49602858+1aman1@users.noreply.github.com>
2019-08-02 13:05:19 +0530

    <algorithm>

--- STL/sort.cpp
+++ STL/sort.cpp
@@ -1,27 +1,32 @@
-// A C++ program to demonstrate STL sort() using
-// our own comparator
-#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
 using namespace std;
  
-// An interval has start time and end time
+// say, An interval 
 struct Interval {
     int start, end;
 };
  
-// Compares two intervals according to staring times.
+// Compares two intervals 
 bool compareInterval(Interval i1, Interval i2) {
     return (i1.start < i2.start);
 }
  
 int main() {
+  int ar[7] = {7,6,5,4,3,2,1};
+  
+  sort(ar, ar+7);
+  cout << "Default sort  : ";
+  for( auto i:ar) cout << i << " ";
+
   Interval arr[] =  { {6,8}, {4,7}, {2,4}, {1,9} };
   int n = sizeof(arr)/sizeof(arr[0]);
 
-  // sort the intervals in increasing order of start time
   // sort(arr, arr+n, greater<int>()); 
   sort(arr, arr+n, compareInterval);
 
-  cout << "Intervals sorted by start time : ";
+  cout << endl;
+  cout << "Custom sort : Intervals sorted by start time : ";
   for (int i = 0; i < n; i++ ) 
     cout << "[" << arr[i].start << "," << arr[i].end << "] ";
 
