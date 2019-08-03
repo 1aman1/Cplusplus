1803ca59f01f82bf2afbc86a84368832397e1dfe
Aman Gupta <49602858+1aman1@users.noreply.github.com>
2019-08-03 12:04:48 +0530

    Update stat&permutation.cpp

--- STL/stat&permutation.cpp
+++ STL/stat&permutation.cpp
@@ -3,57 +3,62 @@
 #include<vector>
 #include<iostream>
 using namespace std;
- 
+
 int main() {
 
-  vector<int> vec(15);
-  int val = 0;
-  
-  for( auto &i:vec ) i = ++val ;
+	vector<int> vec(15);
 
-  for( auto i:vec) cout << i << " " ;
+	/*	iota can replace this code, which makes iota a sequence initialiser
+	int val = 0;
+	for (auto &i : vec) i = ++val;
+	*/
+	iota(vec.begin(), vec.end(), 1);
 
-  cout << "\nmaxelement " << *max_element( vec.cbegin(), vec.cend() );
-  cout << "\nminelement " << *min_element( vec.cbegin(), vec.cend() );
+	/*PRINT(VEC);*/ for (auto i:vec) cout << i << " ";
+	
+	cout << "\nsum : " << accumulate(vec.cbegin(), vec.cend(), 0);
 
-  // numeric solely helps with this function
-  cout << "\nsum : " << accumulate( vec.cbegin(), vec.cend(), 0);
+	cout << "\nmaxelement " << *max_element(vec.cbegin(), vec.cend());
+	cout << "\nminelement " << *min_element(vec.cbegin(), vec.cend());
+	
+	cout << "\ncount occurrence for x : " << count(vec.cbegin(), vec.cend(), 4);
 
-  cout << "\ncount occurrence for x : " << count(vec.cbegin(), vec.cend(), 4);
+	cout << "\nfind an element " << ((find(vec.cbegin(), vec.cend(), 4) == vec.end()) ? "abs" : "pres");
 
-  cout << "\nfind an element " << ((find(vec.cbegin(), vec.cend(), 4) == vec.end() )? "abs" : "pres" );
+	//    ( times, value )
+	vec.assign(5, 50);
+	vec.emplace_back(51);
+	vec.emplace_back(52);
 
-  //    ( times, value )
-  vec.assign(5, 50);
-  vec.emplace_back(51);
-  vec.emplace_back(52);
+	auto l = lower_bound(vec.cbegin(), vec.cend(), 50);
+	cout << "\nlower bound " << l - vec.begin();
 
-  auto l = lower_bound( vec.cbegin(), vec.cend(), 50);
-  cout << "\nlower bound " << l-vec.begin();
+	auto u = upper_bound(vec.cbegin(), vec.cend(), 50);
+	cout << "\nupper bound " << u - vec.begin();
 
-  auto u = upper_bound( vec.cbegin(), vec.cend(), 50);
-  cout << "\nupper bound " << u-vec.begin() ;
+	cout << "\n" << vec.size() << endl;
+	vec.erase(vec.begin());
+	cout << vec.size() << endl;
 
-  cout << "\n" << vec.size() << endl;
-  vec.erase( vec.begin() );
-  cout << vec.size() << endl;
+	vec.assign(1, 1);
+	vec.push_back(200);
+	vec.push_back(3);
 
-  vec.assign(1,1);
-  vec.push_back(200);
-  vec.push_back(3);
-  
-  for( auto i:vec) cout << i << " " ;
+	/*PRINT(VEC);*/ for (auto i:vec) cout << i << " ";
 
-  cout << endl;
-  next_permutation( vec.begin(), vec.end());
-  for( auto i:vec) cout << i << " " ;
+	cout << endl;
+	next_permutation(vec.begin(), vec.end());
+	/*PRINT(VEC);*/ for (auto i:vec) cout << i << " ";
 
-  cout << endl;
-  prev_permutation( vec.begin(), vec.end());
-  for( auto i:vec) cout << i << " " ;
+	cout << endl;
+	prev_permutation(vec.begin(), vec.end());
+	/*PRINT(VEC);*/ for (auto i:vec) cout << i << " ";
 
-  cout << "\ndistance " << distance(vec.begin(), find(vec.begin(), vec.end(), 3));
+	cout << "\ndistance " << distance(vec.begin(), find(vec.begin(), vec.end(), 3));
 
-  cout << endl;
-  return 0;
+	cout << endl;
+	system("pause");
+	return 0;
 }
+// one might give a word for /*PRINT(VEC);*/
+// but this subtly helps in going fast thru the code.
